460e444791357b6683ce08dcc2c50816b6dec88b
rubicman <kubantsev@yandex.ru>
2022-12-06 18:19:10 +0300

    Solve day 6 part two

--- src/main/kotlin/day6/TuningTrouble.kt
+++ src/main/kotlin/day6/TuningTrouble.kt
@@ -3,8 +3,8 @@ package day6
 class TuningTrouble {
     fun startOfTheMessage() {
         val line = readln()
-        mainLoop@ for (i in 3 until line.length) {
-            for (j in i - 3 until i) {
+        mainLoop@ for (i in 13 until line.length) {
+            for (j in i - 13 until i) {
                 for (k in j + 1..i) {
                     if (line[j] == line[k]) continue@mainLoop
                 }
