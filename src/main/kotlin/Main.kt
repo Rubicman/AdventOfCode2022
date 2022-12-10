5ea7780c99e471c4a681295b8cb571e61f248ce6
rubicman <kubantsev@yandex.ru>
2022-12-10 17:59:40 +0300

    Solve day 10 part two

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 import day10.CathodeRayTube
-import day9.RopeBridge
 
 fun main() {
-    CathodeRayTube().simulateCPU()
+    CathodeRayTube().draw()
 }
--- src/main/kotlin/day10/CathodeRayTube.kt
+++ src/main/kotlin/day10/CathodeRayTube.kt
@@ -1,6 +1,7 @@
 package day10
 
 import lines
+import kotlin.math.abs
 
 class CathodeRayTube {
     fun simulateCPU() {
@@ -16,7 +17,7 @@ class CathodeRayTube {
         while (queue.isNotEmpty()) {
             if (i % 40 == 20)
                 sum += i * score
-            when(val command = queue.removeFirst()) {
+            when (val command = queue.removeFirst()) {
                 is StartAdd -> queue.addFirst(EndAdd(command.value))
                 is EndAdd -> score += command.value
                 is Noop -> {}
@@ -26,14 +27,34 @@ class CathodeRayTube {
 
         println(sum)
     }
+
+    fun draw() {
+        val queue = ArrayDeque<Command>()
+
+        lines().map { it.split(" ") }.forEach {
+            queue.addLast(it.takeIf { it[0] == "addx" }?.let { StartAdd(it[1].toLong()) } ?: Noop)
+        }
+
+        var score = 1L
+        val screen = Array(6) { Array(40) { " " } }
+        for (i in 0..239) {
+            screen[i / 40][i % 40] = if (abs(score - i % 40) < 2) "#" else "."
+            when (val command = queue.removeFirstOrNull()) {
+                is StartAdd -> queue.addFirst(EndAdd(command.value))
+                is EndAdd -> score += command.value
+                else -> {}
+            }
+        }
+        screen.forEach { println(it.joinToString("")) }
+    }
 }
 
 sealed interface Command
 
 @JvmInline
-value class StartAdd(val value: Long): Command
+value class StartAdd(val value: Long) : Command
 
 @JvmInline
-value class EndAdd(val value: Long): Command
+value class EndAdd(val value: Long) : Command
 
-object Noop: Command
+object Noop : Command
