ff8fd9b31a96d60ff757a4cfc8099a63b8bc6876
rubicman <kubantsev@yandex.ru>
2022-12-11 15:49:00 +0300

    Solve day 11 part two

--- src/main/kotlin/day11/MonkeyInTheMiddle.kt
+++ src/main/kotlin/day11/MonkeyInTheMiddle.kt
@@ -1,8 +1,6 @@
 package day11
 
 import lines
-import kotlin.collections.ArrayDeque
-import kotlin.concurrent.thread
 
 class MonkeyInTheMiddle {
     fun keepAway() {
@@ -11,21 +9,22 @@ class MonkeyInTheMiddle {
             .windowed(BLOCK_SIZE, BLOCK_SIZE)
             .map { lines ->
                 val items = lines[1].subList(2, lines[1].size).map { it.replace(",", "").toInt() }
-                val leftElement = if (lines[2][3] == "old") Value() else Const(lines[2][3].toInt())
-                val rightElement = if (lines[2][5] == "old") Value() else Const(lines[2][5].toInt())
+                val leftElement = if (lines[2][3] == "old") Value() else Const(Item(lines[2][3].toInt()))
+                val rightElement = if (lines[2][5] == "old") Value() else Const(Item(lines[2][5].toInt()))
                 val operation = when (lines[2][4]) {
                     "+" -> Add(leftElement, rightElement)
                     "*" -> Multiply(leftElement, rightElement)
                     else -> throw IllegalStateException()
                 }
                 val test = lines[3][3].toInt()
+                Item.dividers.add(test)
                 val trueMonkey = lines[4][5].toInt()
                 val falseMonkey = lines[5][5].toInt()
 
                 Monkey(
-                    ArrayDeque<Int>().apply { addAll(items) },
+                    ArrayDeque<Item>().apply { addAll(items.map { Item(it) }) },
                     { operation.calculate(it) },
-                    { if (it % test == 0) trueMonkey else falseMonkey}
+                    { if (it.reminder(test) == 0) trueMonkey else falseMonkey }
                 )
             }.toList()
 
@@ -38,22 +37,22 @@ class MonkeyInTheMiddle {
 
     companion object {
         private const val BLOCK_SIZE = 7
-        private const val ROUNDS = 20
+        private const val ROUNDS = 10_000
     }
 }
 
 class Monkey(
-    private val items: ArrayDeque<Int>,
-    private val map: (Int) -> Int,
-    private val nextMonkey: (Int) -> Int,
+    private val items: ArrayDeque<Item>,
+    private val map: (Item) -> Item,
+    private val nextMonkey: (Item) -> Int,
 ) {
-    var inspectations = 0
+    var inspectations = 0L
 
     fun throwItems(monkeys: List<Monkey>) {
         while (items.isNotEmpty()) {
             inspectations++
             var item = items.removeFirst()
-            item = map(item) / 3
+            item = map(item)
             val monkeyIndex = nextMonkey(item)
             monkeys[monkeyIndex].items.addLast(item)
         }
@@ -61,27 +60,56 @@ class Monkey(
 }
 
 interface Element {
-    fun calculate(x: Int): Int
+    fun calculate(x: Item): Item
 }
 
 class Add(
     private val left: Element,
     private val right: Element,
 ) : Element {
-    override fun calculate(x: Int): Int = left.calculate(x) + right.calculate(x)
+    override fun calculate(x: Item): Item = left.calculate(x) + right.calculate(x)
 }
 
 class Multiply(
     private val left: Element,
     private val right: Element,
 ) : Element {
-    override fun calculate(x: Int): Int = left.calculate(x) * right.calculate(x)
+    override fun calculate(x: Item): Item = left.calculate(x) * right.calculate(x)
 }
 
-class Const(private val value: Int) : Element {
-    override fun calculate(x: Int): Int = value
+class Const(private val value: Item) : Element {
+    override fun calculate(x: Item): Item = value
 }
 
 class Value : Element {
-    override fun calculate(x: Int): Int = x
+    override fun calculate(x: Item): Item = x
+}
+
+class Item private constructor(
+    private val reminders: MutableMap<Int, Int> = mutableMapOf(),
+) {
+    
+    constructor(value: Int): this(mutableMapOf<Int, Int>().withDefault { value % it })
+    
+    operator fun plus(other: Item): Item {
+        val newReminders = mutableMapOf<Int, Int>()
+        for (divider in dividers) {
+            newReminders[divider] = (reminder(divider) + other.reminder(divider)) % divider
+        }
+        return Item(newReminders)
+    }
+
+    operator fun times(other: Item): Item {
+        val newReminders = mutableMapOf<Int, Int>()
+        for (divider in dividers) {
+            newReminders[divider] = (reminder(divider) * other.reminder(divider)) % divider
+        }
+        return Item(newReminders)
+    }
+
+    fun reminder(divider: Int): Int = reminders.getValue(divider)
+    
+    companion object {
+        val dividers = mutableListOf<Int>()
+    }
 }
