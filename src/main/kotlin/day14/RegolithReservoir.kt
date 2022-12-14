fa8c80db8996dd26386c01c6a6a166b197fe0689
rubicman <kubantsev@yandex.ru>
2022-12-14 12:54:09 +0300

    Solve day 14 part two

--- src/main/kotlin/day14/RegolithReservoir.kt
+++ src/main/kotlin/day14/RegolithReservoir.kt
@@ -27,12 +27,13 @@ class RegolithReservoir {
             .reduce { (a1, b1), (a2, b2) -> minOf(a1, a2) to minOf(b1, b2) }
 
         val cave =
-            Array(maxCoordinates.first - minCoordinates.first + 3) { Array(maxCoordinates.second - minCoordinates.second + 3) { 0 } }
+            Array(LENGTH) { Array(maxCoordinates.second + 4) { 0 } }
 
         paths
-            .forEach { path ->
+            .plus(listOf(listOf(minCoordinates.first - LENGTH / 2 to maxCoordinates.second + 2, minCoordinates.first + LENGTH / 2 - 1 to maxCoordinates.second + 2)))
+            .forEach { path: List<Pair<Int, Int>> ->
                 path
-                    .map { (x, y) -> x - minCoordinates.first + 1 to y - minCoordinates.second + 1 }
+                    .map { (x, y) -> x - minCoordinates.first + LENGTH / 2 to y - minCoordinates.second + 1 }
                     .zipWithNext()
                     .forEach { (start, end) ->
                         if (start.first == end.first) {
@@ -45,11 +46,13 @@ class RegolithReservoir {
 
         var k = -1
         mainLoop@ while (true) {
-            var x = START_POINT.first - minCoordinates.first + 1
+            var x = START_POINT.first - minCoordinates.first + LENGTH / 2
             var y = START_POINT.second - minCoordinates.second + 1
             k++
+            if (cave[x][y] != 0)
+                break
 
-            while (y < cave[0].size - 1) {
+            while (true) {
                 when {
                     cave[x][y + 1] == 0 -> {}
                     cave[x - 1][y + 1] == 0 -> x--
@@ -61,16 +64,38 @@ class RegolithReservoir {
                 }
                 y++
             }
-
-            break
         }
 
+        cave.print()
         println(k)
     }
 
     private fun range(a: Int, b: Int) = (minOf(a, b)..maxOf(a, b))
 
+    private fun Array<Array<Int>>.print() = transpose()
+        .forEach { line ->
+            line.joinToString("") {
+                when (it) {
+                    0 -> "."
+                    1 -> "o"
+                    2 -> "#"
+                    else -> "?"
+                }
+            }.let(::println)
+        }
+
+    private inline fun <reified T> Array<Array<T>>.transpose(): Array<Array<T>> {
+        val cols = get(0).size
+        val rows = size
+        return Array(cols) { j ->
+            Array(rows) { i ->
+                get(i)[j]
+            }
+        }
+    }
+
     companion object {
         private val START_POINT = 500 to 0
+        private const val LENGTH = 1_000
     }
 }
