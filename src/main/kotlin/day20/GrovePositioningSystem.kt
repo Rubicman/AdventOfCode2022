2b5d2a863f6c23bb3c5bf1edd6ff66027aa74829
rubicman <kubantsev@yandex.ru>
2022-12-20 21:01:52 +0300

    Solve day 20 part two

--- src/main/kotlin/day20/GrovePositioningSystem.kt
+++ src/main/kotlin/day20/GrovePositioningSystem.kt
@@ -5,9 +5,19 @@ import lines
 class GrovePositioningSystem {
     fun decode() {
         val list = lines().toList()
-            .mapIndexed { index, s -> index to s.toInt() }
+            .mapIndexed { index, s -> index.toLong() to s.toLong() * SECRET_KEY}
             .toMutableList()
 
+        repeat(DECODE_TIMES) {
+            decode(list)
+        }
+
+        list.sortBy { it.first }
+        val zeroIndex = list.indexOfFirst { it.second == 0L }
+        println(CONTROL_POINTS.map { (it + zeroIndex) % list.size }.sumOf { list[it].second })
+    }
+
+    private fun decode(list: MutableList<Pair<Long, Long>>) {
         for (i in list.indices) {
             var newIndex = list[i].first + list[i].second
             for (j in list.indices) {
@@ -15,11 +25,9 @@ class GrovePositioningSystem {
                     list[j] = list[j].first - 1 to list[j].second
                 }
             }
+            newIndex %= list.size - 1
             while (newIndex <= 0) {
                 newIndex += list.size - 1
-            }
-            while (newIndex >= list.size) {
-                newIndex -= list.size - 1
             }
             for (j in list.indices) {
                 if (list[j].first >= newIndex) {
@@ -28,15 +36,11 @@ class GrovePositioningSystem {
             }
             list[i] = newIndex to list[i].second
         }
-
-        list.sortBy { it.first }
-
-        val zeroIndex = list.indexOfFirst { it.second == 0 }
-
-        println(CONTROL_POINTS.map { (it + zeroIndex) % list.size }.map { list[it].second }.sum())
     }
 
     companion object {
         private val CONTROL_POINTS = setOf(1000, 2000, 3000)
+        private const val DECODE_TIMES = 10
+        private const val SECRET_KEY = 811589153L
     }
 }
